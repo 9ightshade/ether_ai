93f888a5d3d273e3f23de89a494d7f1cdf535ca6
9ightshade <aniekemeusanga@gmail.com>
2025-04-16 20:31:18 +0100

    community carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import GridCarousel from "@/components/community/communityCarousel";
 
 
 export default function Home() {
   return (
     <div className="bg-[#000000]">
-    
+    <GridCarousel/>
     </div>
   );
 }
